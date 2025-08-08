40b33ade53bbc3592e3a223089074cd2f6ca150d
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-07 22:21:51 -0300

    Criação de projeto no Visual Studio

---
+++ 04 _Vetores/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace _04__Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word"); 
+        }
+    }
+}
