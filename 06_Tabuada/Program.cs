4618185212d5e8c5ed8e71f9b946fea6397c7fde
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-08 22:05:33 -0300

    Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+     
+            {
+                int multiplicador, multiplicando = 0, resultado = 0;
+
+                Console.WriteLine("Digite um número inteiro: ");
+                multiplicador = int.Parse(Console.ReadLine());
+
+                while (multiplicando <= 10)
+                {
+                    resultado = multiplicador * multiplicando;
+                    Console.WriteLine($"{multiplicador} X {multiplicando} = {resultado}");
+                    multiplicando++;
+                }
+            }
+        }
+    }
+
