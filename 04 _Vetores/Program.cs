b8093b5217c02f04a3dc745f6e3d6325f9b3ec19
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-08 21:42:14 -0300

    Loop While

--- 04 _Vetores/Program.cs
+++ 04 _Vetores/Program.cs
@@ -7,7 +7,60 @@ namespace _04__Vetores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word"); 
+            //declaração de variável
+            // sintaxe: tipo nomeVariável = valor
+            string nome = "Fulano de Tal";
+
+            //reatribuição de valor em uma variável
+            nome = "Beltrano";
+            Console.WriteLine(nome);
+
+            //declaração de vetores
+            // sintaxe: tipo[] nomeVariavel = {valor1, valor2, valor3,};
+            string[] nomes = { "Fulano de tal", "Beltrano", "Sicrano", "João", "José", "Maria"};
+            Console.WriteLine(nomes[0]);
+            Console.WriteLine(nomes[1]);
+            Console.WriteLine(nomes[2]);
+
+            //Loop FOR
+            // Sintaxe: for(indice; controle; incremento)
+            for (int i = 0; i < nomes.Length; i++) 
+            {
+                Console.WriteLine("{0}º Nome: {1}", i+1, nomes[i]);
+            }
+
+            //Impressão dos 100 primeiros números pares
+            for (int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine("Número: {0}", i);
+            }
+            // Loop FOREACH
+            // Sintaxe: foreach(variavel in vetor)
+            foreach (string varNome in nomes)
+            {
+                Console.WriteLine("Nome: {0}", varNome);
+            }
         }
     }
 }
+/*
+ * Ctrl + .: Abre a janela de exceções, permitindo que você configure o Visual Studio para parar em exceções específicas. 
+
+Ctrl + K, Ctrl + C: Comenta a seleção de código. 
+
+Ctrl + K, Ctrl + U: Descomenta a seleção de código. 
+
+F12: Vai para a definição do objeto onde o cursor está posicionado. 
+
+F9: Adiciona ou remove um ponto de interrupção na linha de código atual. 
+
+Ctrl + Shift + B: Compila a solução. 
+
+Ctrl + F5: Inicia a depuração sem depurar. 
+
+Ctrl + S: Salva o arquivo atual. 
+
+Ctrl + Shift + N: Cria um novo projeto. 
+
+Ctrl + Shift + O: Abre um arquivo existente. 
+*/
---
+++ 05 _ Loops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _05___Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação de um vetor para armazenamento de 100 elementos
+            //string [] nomes = {"Fulano","Beltrano","Sicrano"};
+            string[] nomes = new string [100];
+            string continuar = "S";
+
+            //será meu índice para atribuição no vetor
+            int contador = 0;
+
+            //Loop While
+            //Sintaxe: while (expressão booleana)
+            //método ToUper():  converte o texto para maiúsculo 
+
+            while (continuar .ToUpper() == "S")
+            {
+                Console.WriteLine("Digite o {0}° nome: ", contador+1);
+                //Append: adiciona um ítem no vetor
+                nomes[contador] = Console.ReadLine();
+
+                //incrementar o contador
+                contador++;
+                Console.WriteLine("Deseja continuar? S/N");
+                continuar = Console.ReadLine();
+            }
+            Console.WriteLine("Nomes informados:");
+            foreach (string str in nomes)
+            {
+                //!= significa diferente
+                if (str != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
