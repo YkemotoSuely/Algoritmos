3b8a1a5540d7c73169e0aedfc7d74754e52b636f
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-11 21:53:33 -0300

    Calcula tempo de download.

---
+++ 07_TempoDownload/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            double tamanhoMB, velocidadeMbps;
+
+            Console.WriteLine("Digite o tamanho do seu arquivo em megabytes: ");
+            string valorDigitado = Console.ReadLine();
+
+            if (double.TryParse(valorDigitado, out tamanhoMB) && tamanhoMB > 0)
+            {
+                Console.WriteLine("Digite a velocidade da sua internet em Mbps: ");
+                valorDigitado = Console.ReadLine();
+
+                if (double.TryParse(valorDigitado, out velocidadeMbps) && velocidadeMbps > 0)
+                {
+                    double tempoSegundos = (tamanhoMB * 8) / velocidadeMbps;
+                    double tempoMinutos = tempoSegundos / 60;
+
+                    Console.WriteLine($"Tempo aproximado de download: {tempoMinutos:F2} minutos");
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido! Digite um valor maior que zero.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido! Digite um valor maior que zero.");
+            }
+        }
+    }
+
