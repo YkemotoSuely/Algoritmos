80d9f94f3bb6fdf870f5cc02d663c64f9d07acf1
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-08-25 22:24:13 -0300

    Inserção dos feriados móveis

--- 09_Calendario/Program.cs
+++ 09_Calendario/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.Cryptography.X509Certificates;
+
 
 
 namespace _09_Calendario
@@ -95,9 +94,6 @@ namespace _09_Calendario
         }
 
 
-
-
-
         public static int[] RetornaFeriados(int mes, int ano)
         {
             int[] feriados = new int[15];
@@ -107,7 +103,7 @@ namespace _09_Calendario
             //feriados[indice++] = 21;
 
             if (mes == 1) feriados[indice++] = 1;
-            
+
             else if (mes == 4)
             {
                 feriados[indice++] = 4; //Aniversário de Marília
@@ -133,14 +129,105 @@ namespace _09_Calendario
                 feriados[indice++] = 25; // Natal
             }
 
+
+            // Feriados móveis
+            DateTime pascoa = CalcularPascoa(ano);
+            DateTime carnaval = pascoa.AddDays(-47);
+            DateTime sextaSanta = pascoa.AddDays(-2);
+            DateTime corpusChristi = pascoa.AddDays(60);
+
+            if (pascoa.Month == mes) feriados[indice++] = pascoa.Day;
+            if (carnaval.Month == mes) feriados[indice++] = carnaval.Day;
+            if (sextaSanta.Month == mes) feriados[indice++] = sextaSanta.Day;
+            if (corpusChristi.Month == mes) feriados[indice++] = corpusChristi.Day;
+
             return feriados;
+        }
 
 
- 
+        public static DateTime CalcularPascoa(int ano)
+        {
+            int X = 0, Y = 0;
 
+            // Ajustes históricos conforme o século
+            if (ano <= 1699) { X = 22; Y = 2; }
+            else if (ano <= 1799) { X = 23; Y = 3; }
+            else if (ano <= 1899) { X = 24; Y = 4; }
+            else if (ano <= 2099) { X = 24; Y = 5; }
+            else if (ano <= 2199) { X = 24; Y = 6; }
+            else if (ano <= 2299) { X = 24; Y = 7; }
+
+            // Cálculos do algoritmo de Meeus/Jones/Butcher
+            int a = ano % 19;
+            int b = ano % 4;
+            int c = ano % 7;
+            int d = (19 * a + X) % 30;
+            int g = (2 * b + 4 * c + 6 * d + Y) % 7;
+
+            int dia;
+            int mes;
+
+            // Determina se a Páscoa cai em março ou abril
+            if ((d + g) > 9)
+            {
+                dia = d + g - 9;
+                mes = 4; // Abril
+            }
+            else
+            {
+                dia = d + g + 22;
+                mes = 3; // Março
+            }
+
+            // Retorna a data da Páscoa
+            return new DateTime(ano, mes, dia);
         }
     }
 }
+
+        /* Método que calcula a data da Páscoa
+        public static DateTime CalcularPascoa(int ano)
+        {
+           
+
+            int X = 0, Y = 0;
+
+            if (ano <= 1699) { X = 22; Y = 2; }
+            else if (ano <= 1799) { X = 23; Y = 3; }
+            else if (ano <= 1899) { X = 24; Y = 4; }
+            else if (ano <= 2099) { X = 24; Y = 5; }
+            else if (ano <= 2199) { X = 24; Y = 6; }
+            else if (ano <= 2299) { X = 24; Y = 7; }
+
+            int a = ano % 19;
+            int b = ano % 4;
+            int c = ano % 7;
+            int d = (19 * a + X) % 30;
+            int g = (2 * b + 4 * c + 6 * d + Y) % 7;
+
+            int dia, mes;
+
+            if ((d + g) > 9)
+            {
+                dia = d + g - 9;
+                mes = 4;
+            }
+            else
+            {
+                dia = d + g + 22;
+                mes = 3;
+            }
+
+            return new DateTime(ano, mes, dia);
+        }
+ }*/
+
+
+
+
+
+
+    
      
 
 
